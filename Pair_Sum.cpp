16c38dab8d64d6be6c24578cd68dff644bfaa85a
ProgrammerAtik <atik.khan3477@gmail.com>
2025-11-14 12:31:46 +0600

    Update

---
+++ Pair_Sum.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> pairsum(vector<int>nums,int target)
+{
+    vector<int>ans;
+    int start=0;
+    int end=nums.size()-1;
+    while(start<end)
+    {
+        if(nums[start]+nums[end]>target)
+        {
+            end--;
+        }
+        else if(nums[start]+nums[end]<target)
+        {
+            start++;
+        }
+        else
+        {
+            ans.push_back(start);
+            ans.push_back(end);
+            return ans;
+        }
+    }
+    return ans;
+}
+int main()
+{
+    vector<int>nums={2,7,11,15};
+    int target=9;
+    vector<int>answer=pairsum(nums,target);
+    cout<<answer[0]<<endl<<answer[1]<<endl;
+    return 0;
+}
