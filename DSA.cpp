f0550882b89f152ee03ad761aeb7b50e799b0c54
Atik2405130 <atik.khan3477@gmail.com>
2025-12-15 19:53:04 +0600

    Update

--- DSA.cpp
+++ DSA.cpp
@@ -37,23 +37,63 @@
 //     cout<<arr[imax]<<arr[ismax]<<endl;
 // }
 
-#include<iostream>
-#include<vector>
-using namespace std;
-int main()
-{
-    vector<int>vec;//0
-    //vec.push_back(25);
-    //cout<<"size= "<<vec.size()<<endl;
-    vec.push_back(0);//Insert Value
-    vec.push_back(1);
-    vec.push_back(2);
-    vec.push_back(3);
-    vec.push_back(4);//Capacity doubled in each case not size(elements)
-    cout<<"size = "<<vec.size()<<endl;
-    cout<<vec.capacity()<<endl;// Capacity 
-    //vec.pop_back();//delete element from the last
-    //for(int val:vec){
-    //cout<<val<<endl;
-    //}
-}
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main()
+// {
+//     vector<int>vec;//0
+//     //vec.push_back(25);
+//     //cout<<"size= "<<vec.size()<<endl;
+//     vec.push_back(0);//Insert Value
+//     vec.push_back(1);
+//     vec.push_back(2);
+//     vec.push_back(3);
+//     vec.push_back(4);//Capacity doubled in each case not size(elements)
+//     cout<<"size = "<<vec.size()<<endl;
+//     cout<<vec.capacity()<<endl;// Capacity 
+//     //vec.pop_back();//delete element from the last
+//     //for(int val:vec){
+//     //cout<<val<<endl;
+//     //}
+// }
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main()
+// {
+//     int n=5;
+//     int arr[]={1,2,3,4,5};
+//     for(int start=0;start<n;start++)
+//     {
+//         for(int end=start;end<n;end++)
+//         {
+//             for(int i=start;i<=end;i++)
+//             {
+//                 cout<<arr[i];
+//             }
+//             cout<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main()
+// {
+//     int n=5;
+//     int arr[5]={1,2,3,4,5};
+//     int maxSum=INT_MIN;
+//     for(int start=0;start<n;start++)
+//     {
+//         int currsum=0;
+//         for(int end=start;end<n;end++)
+//         {
+//             currsum+=arr[end];
+//             maxSum=max(currsum,maxSum);
+//         }
+//     }
+//     cout<<"max subarray sum = "<<maxSum<<endl;
+// }
