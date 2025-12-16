49c7788769d96aa6e0acadcac5c18ececc018db9
ProgrammerAtik <atik.khan3477@gmail.com>
2025-12-16 12:45:09 +0600

    update

---
+++ LinkedList/LinkedListPush_front_Back+Pop_Front_Bacl.cpp
@@ -0,0 +1,98 @@
+#include<iostream>
+using namespace std;
+
+// Individual Node CLass //
+
+class Node
+{
+    public:
+    int data;
+    Node* next;
+    Node(int value)
+    {
+        data=value;
+        next=nullptr;
+    }
+};
+
+class List
+{
+    Node *head;
+    Node *tail;
+    public:
+        List(){
+            head=tail=NULL;
+        }
+        void push_front(int value)
+        {
+            Node *current=new Node(value);
+            if(head==NULL)head=tail=current; //trail upholds//
+            else
+            {
+                current->next=head;
+                head=current;
+            }
+        }
+        void push_back(int value)
+        {
+        Node *n2=new Node(value);
+        if(head==NULL)head=tail=n2;
+        else
+        {
+            tail->next=n2;
+            tail=n2;
+        }
+        }
+        void printLL()
+        {
+            Node *temp=head;
+            while(temp)
+            {
+                cout<<temp->data<<endl;
+                temp=temp->next;
+            }
+        }
+        void pop_front()
+        {
+            if(head==nullptr)
+            {
+                cout<<"error"<<endl;
+                return;
+            }
+            Node *temp=head;
+            head=head->next;
+            temp->next=nullptr;
+            delete temp;
+        }
+        void pop_back()
+        {
+            if(head==NULL)return;
+            Node *temp=head;
+            while(temp->next!=tail)
+            {
+                temp=temp->next;
+            }
+            temp->next=nullptr;
+            delete tail;
+            tail=temp;
+        }
+};
+
+int main()
+{
+    List ll;
+    ll.push_front(1);
+    ll.push_front(2);
+    ll.push_front(3);
+    ll.push_front(4);
+    ll.push_front(5);
+    ll.push_front(6);
+    ll.push_front(7); // 3 2 1
+    ll.printLL();
+    ll.pop_front();
+    cout<<"After deleting the first element :"<<endl;
+    ll.printLL();
+    ll.pop_back();
+     cout<<"After deleting the last element :"<<endl;
+    ll.printLL();
+}
