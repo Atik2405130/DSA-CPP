b7edb505667a2d6d4c8479341b47d3b6d6b21f87
ProgrammerAtik <atik.khan3477@gmail.com>
2026-01-03 19:15:18 +0600

    Update

--- LinkedList/Reverse.cpp
+++ LinkedList/Reverse.cpp
@@ -4,8 +4,11 @@ using namespace std;
  struct ListNode {
       int val;
       ListNode *next;
+      ListNode *prev;
+      ListNode *head;
+      ListNode *tail;
       ListNode() : val(0), next(nullptr) {}
-      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr),prev(nullptr) {}
       ListNode(int x, ListNode *next) : val(x), next(next) {}
     };
 class Solution {
@@ -24,4 +27,34 @@ public:
         }
         return prev;
     }
+    void reverse(ListNode *head,ListNode *tail)
+    {
+    ListNode* cur = head;
+    ListNode* temp = NULL;
+
+    // Traverse the list and swap next/prev for each node
+    while (cur != NULL)
+    {
+        temp = cur->prev;
+        cur->prev = cur->next; //Just Pointer direction Changing
+        cur->next = temp;
+
+        // Move to the "next" node (which is prev after swap)
+        cur = cur->prev;
+    }
+
+    // After loop, temp points to the old head's prev
+    if (temp != NULL)
+    {
+        head = temp->prev;  // new head
+    }
+    // Update tail as well
+    tail = head;
+    while (tail && tail->next != NULL)
+    {
+        tail = tail->next;
+    }
+    }
 };
+
+//Doubly Linked List
