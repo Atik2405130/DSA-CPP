ff2d76abd061b65767732bb2f0bcb34f7eb25fab
ProgrammerAtik <atik.khan3477@gmail.com>
2026-01-28 23:20:42 -0800

    update

---
+++ BinaryTree/Diameter.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+  //Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+public:
+    int ans=0;
+    int height(TreeNode* root){
+        if(root==NULL)return 0;
+        int lefth=height(root->left);
+        int righth=height(root->right);
+        ans=max(ans,lefth+righth);
+        return max(lefth,righth)+1;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        height(root);
+        return ans;
+    }
+};
