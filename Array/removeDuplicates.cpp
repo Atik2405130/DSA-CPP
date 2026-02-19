954487314f935dbbf77d6b97bde39fb4fd977e40
ProgrammerAtik <atik.khan3477@gmail.com>
2026-02-19 02:00:23 -0800

    newcode

---
+++ Array/removeDuplicates.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i=0;
+        for(int j=0;j<nums.size();j++){
+            if(nums[i]!=nums[j]){
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        return i+1;
+    }
+};
