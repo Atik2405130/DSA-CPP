580da25e2af6cffffb53e47e75617904f5a2074f
ProgrammerAtik <atik.khan3477@gmail.com>
2026-03-06 23:27:43 -0800

    sortingAdd

---
+++ Array/BubbleSort.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void bubbleSort(int arr[],int n){ // --->O(n^2)
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1])swap(arr[j],arr[j+1]);
+        }
+    }
+}
+int main(){
+    int n=5;
+    int arr[]={4,1,5,2,3};
+    bubbleSort(arr,n);
+    for(int v:arr){
+        cout<<v<<" ";
+    }
+}
